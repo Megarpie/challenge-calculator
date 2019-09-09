c33e24395d8a88fd88cde9e11707510595869687
Megarpie <megankim314@gmail.com>
2019-09-09 15:03:41 -0700

    Step 2: Add unlimited number of numbers

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const rl = readline.createInterface({
 });
 
 /*
-Step 1:
-Support a maximum of 2 numbers using a comma delimiter
+Step 2:
+Support an unlimited number of numbers
 */
-const addTwo = (str) => {
+const addStringCalculator = (str) => {
   // check if string is empty first, return 0 if so
   if (!str) {
     return 0;
@@ -20,8 +20,7 @@ const addTwo = (str) => {
   // convert string into number array
   const numArray = str.split(',').map(Number);
 
-  // // max of two numbers only
-  return numArray.slice(0,2).reduce((x,y) => numberCheck(x) + numberCheck(y), 0);
+  return numArray.reduce((x,y) => numberCheck(x) + numberCheck(y), 0);
 }
 
 // checks if number is NaN, return 0 if so
@@ -32,11 +31,11 @@ const numberCheck = (num) => {
 rl.prompt();
 
 rl.on('line', (line) => {
-  const number = addTwo(line);
+  const number = addStringCalculator(line);
   console.log('Your sum is: ', number);
   process.exit(0);
 }).on('close', () => {
   process.exit(0);
 });
 
-exports.addTwo = (str) => addTwo(str);
+exports.add = (str) => addStringCalculator(str);
--- test/testCalc.js
+++ test/testCalc.js
@@ -1,23 +1,26 @@
 const assert = require('assert');
 const calculator = require('../index');
 
-describe('#1: Add max of two numbers using a comma delimiter', () => {
+describe('#2: Add unlimited number of numbers', () => {
   it('should return 0 if empty string', () => {
-    assert.equal(calculator.addTwo(''), 0);
+    assert.equal(calculator.add(''), 0);
   });
   it('should add two numbers', () => {
-    assert.equal(calculator.addTwo('3,6'), 9);
+    assert.equal(calculator.add('3,6'), 9);
   });
   it('should return single number with one number input', () => {
-    assert.equal(calculator.addTwo('20'), 20);
+    assert.equal(calculator.add('20'), 20);
   });
   it('should add two numbers with one invalid string', () => {
-    assert.equal(calculator.addTwo('coffee, 50'), 50);
+    assert.equal(calculator.add('coffee, 50'), 50);
   });
   it('should add two numbers with two invalid strings', () => {
-    assert.equal(calculator.addTwo('milk, tea'), 0);
+    assert.equal(calculator.add('milk, tea'), 0);
   });
-  it('should add only first two when more than two numbers inputed', () => {
-    assert.equal(calculator.addTwo('4,5,6'), 9);
+  it('should add unlimted number of numbers', () => {
+    assert.equal(calculator.add('1,2,3,4,5,6,7,8,9,10'), 55);
+  });
+  it('should add unlimted number of numbers with invalid strings', () => {
+    assert.equal(calculator.add('juice,2,3,4,horchata,6,7,8,9,10'), 49);
   });
 });
