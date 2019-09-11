5875ca4d83788304f81124f78cb3150853f0baa9
Megarpie <megankim314@gmail.com>
2019-09-10 21:36:01 -0700

    fix/add tests

--- test/testCalc.js
+++ test/testCalc.js
@@ -24,7 +24,7 @@ describe('#1: Support a maximum of 2 numbers using a comma delimiter', () => {
   // });
 });
 
-describe('#2: Support a maximum of 2 numbers using a comma delimiter', () => {
+describe('#2: Support an unlimited number of numbers', () => {
   it('should add unlimted number of numbers', () => {
     assert.equal(calculator.add('1,2,3,4,5,6,7,8,9,10'), 55);
   });
@@ -39,6 +39,9 @@ describe('#3: Support newline character as an alternative delimiter', () => {
   });
   it('should add numbers with combination of commas and newlines', () => {
     assert.equal(calculator.add('2,4\\n6\\n8'), 20);
+  });
+  it('should add numbers with combination of commas and newlines and invalid strings', () => {
+    assert.equal(calculator.add('1,3,sugar\\n20'), 24);
   });
 });
 
@@ -96,6 +99,9 @@ describe('#7: Support 1 custom delimiter of any length', () => {
 describe('#8: Support multiple delimiters of any length', () => {
   it('should add numbers with multiple delimiters', () => {
     assert.equal(calculator.add('//[#][!!]\\n1!!2#3'), 6);
+  });
+  it('should add numbers with multiple delimiters', () => {
+    assert.equal(calculator.add('//[*][!!][r9r]\\n11r9r22*33!!44'), 110);
   });
   it('should add numbers with multiple delimiters and invalid strings', () => {
     assert.equal(calculator.add('//[@;][f3f][!]\\n1!1,1@;1f3f1\\n1!kirby'), 6);
